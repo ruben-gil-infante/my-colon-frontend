338cd81473e97475195ee75ebf16c1a739cc249e
rgili <rgili@edu.tecnocampus.cat>
2020-05-15 19:53:13 +0200

    Canviats els estils dels signes vitals

--- src/app/pages/signes-vitals/signes-vitals.page.ts
+++ src/app/pages/signes-vitals/signes-vitals.page.ts
@@ -14,7 +14,7 @@ export class SignesVitalsPage implements OnInit {
     {tipus: 1, nom: 'Activitat', descripcio: 'Passos', valor: 0, img: "../../../assets/icon/person-run.svg"},
     {tipus: 2, nom: 'Tensió arterial', descripcio: 'mmHg', valor: 0, img: "../../../assets/icon/blood-pressure.svg"},
     {tipus: 3, nom: 'Glicèmies', descripcio: 'mg/dl', valor: 0, img: "../../../assets/icon/hemoglobin-test-meter.svg"},
-    {tipus: 4, nom: 'Freqüència cardaca', descripcio: 'bpm', valor: 0, img: "../../../assets/icon/heart-frequency.svg"},
+    {tipus: 4, nom: 'Freqüència cardiaca', descripcio: 'bpm', valor: 0, img: "../../../assets/icon/heart-frequency.svg"},
     {tipus: 5, nom: 'Temperatura', descripcio: 'ºC', valor: 0, img: "../../../assets/icon/thermometer.svg"},
     {tipus: 6, nom: 'Pes', descripcio: 'Kg', valor: 0, img: "../../../assets/icon/balance.svg"},
   ];
