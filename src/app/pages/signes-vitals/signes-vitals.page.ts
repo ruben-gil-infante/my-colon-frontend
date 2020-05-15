33f562d83bc366644908153b297ed973eb75d1b9
rgili <rgili@edu.tecnocampus.cat>
2020-05-15 12:43:15 +0200

    Signes vitals finalitzat

--- src/app/pages/signes-vitals/signes-vitals.page.ts
+++ src/app/pages/signes-vitals/signes-vitals.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { SigneVitalItem } from 'src/interfaces/interfaces';
+import { DataService } from 'src/app/services/data.service';
 
 @Component({
   selector: 'app-signes-vitals',
@@ -8,6 +9,7 @@ import { SigneVitalItem } from 'src/interfaces/interfaces';
 })
 export class SignesVitalsPage implements OnInit {
 
+  endpoint: string = '/api/v1/signesvitals';
   signesVitals: SigneVitalItem [] = [
     {tipus: 1, nom: 'Activitat', descripcio: 'Passos', valor: 0, img: "../../../assets/icon/person-run.svg"},
     {tipus: 2, nom: 'Tensió arterial', descripcio: 'mmHg', valor: 0, img: "../../../assets/icon/blood-pressure.svg"},
@@ -17,9 +19,28 @@ export class SignesVitalsPage implements OnInit {
     {tipus: 6, nom: 'Pes', descripcio: 'Kg', valor: 0, img: "../../../assets/icon/balance.svg"},
   ];
 
-  constructor() { }
+  constructor(private dataSerivce : DataService) { }
 
   ngOnInit() {
   }
 
+  async guardarSigneVital ( event ){
+    let signeVitalForm = {
+      usuari: this.dataSerivce.getUsuariId(),
+      valor: event.valor,
+      tipus: event.tipus,
+      data: this.dataSerivce.getCurrentDate(),
+    };
+    
+    (await this.dataSerivce.submit(this.endpoint, signeVitalForm)).subscribe(
+      data => {
+        this.dataSerivce.loadingControllerDismiss();
+        this.dataSerivce.presentToast("Guardat amb èxit...");
+      },
+      error => {
+        this.dataSerivce.loadingControllerDismiss();
+        this.dataSerivce.presentToast("Error guardant...");
+      }
+    );
+  }
 }
